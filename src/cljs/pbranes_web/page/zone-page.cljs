f1d666ec900eecbd813841e6f926665e6296c6d5
enkidu36 <36429032+enkidu36@users.noreply.github.com>
2018-03-26 19:29:09 -0500

    Add files via upload

--- src/cljs/pbranes_web/page/zone-page.cljs
+++ src/cljs/pbranes_web/page/zone-page.cljs
@@ -1,76 +1,54 @@
 (ns pbranes-web.page.zone-page
   (:require [reagent.core :as reagent :refer [atom]]
             [pbranes-web.zones :as zones]
-            [clojure.string :as s]
             [ajax.core :refer [GET POST]]))
 
-(defonce ftp-state (reagent/atom {:ftp 0 :method zones/coggin}) )
+(defonce ftp (reagent/atom 0))
 
-(defn get-ftp-value [e]
-  (let [ x (-> e .-target .-value)]
+(defn ftp-input []
+  (let [ x (.-value (js/document.getElementById "test-value"))]
     (if (nil? x) 0  x)))
 
-(defn get-method-value [e]
-  (let [x (-> e .-target .-value)]
-    (if (= x ":friel") zones/friel zones/coggin)))
-
-(defn swap-value!
-  [param value]
-  (swap! ftp-state assoc param value))
-
-(defn ftp-component []
-  [:div#ftp-value.col-sm-3
-   [:div.form-group
+(defn input-comp []
+  [:div#ftp-value
+   [:div.form-group.col-sm-6
     [:label.control-label {:for "test-value"} "FTP value: "]
-    [:input#test-value.form-control.input-sm-1 {:type "text"
-                                              :placeholder "Enter FTP test value"
-                                              :on-blur #(swap-value! :ftp (get-ftp-value %))
-                                              :on-keyPress #(when (=  (.-charCode  %) 13)
-                                                                (swap-value! :ftp (get-ftp-value %)))}]]
-  [:div.form-group
-   [:label.control-label {:for "method-value"} "Method Selection:"]
-   [:select.form-control.col-sm-3 {
-                                   :on-change #(swap-value! :method (get-method-value %))}
-    [:option {:value ":coggin" :active true} "Coggin Method"]
-    [:option {:value ":friel"} "Friel Method"]]]])
-
-(defn method-name-text []
-  (str (:method-name  (:method @ftp-state)) " method"))
-
-(defn ftp-text []
-  (let [val (:ftp @ftp-state)
-        prefix "Current FTP"]
-    (if (or (s/blank? val) (zero? (int val)))
-      prefix
-      (str prefix " ( " val " watts   )" ))))
+      [:input#test-value.form-control.input-sm {:type "text"
+                                                :placeholder "Enter FTP test value"
+                                                :on-blur #(reset! ftp (ftp-input))
+                                                :on-keyPress #(when (=  (.-charCode  %) 13)
+                                                                  (reset! ftp (ftp-input)))}]]])
 
 (defn table [input]
-  [:div.row
-   [:div.col-sm-8
-    [:table.table.table-striped.table-bordered.table-center
-      [:thead.footer
-       [:tr
-        [:th (ftp-text) ]
-        [:th {:colSpan "3"} (method-name-text)]]
-       [:tr
-        [:th.table-center "Zone-Purpose"]
-        [:th.table-center "Lower"]
-        [:th.table-center "Upper"]
-        [:th.table-center  "Percentage of FTP"]]]
-      [:tbody
-        (for [row input
-              :let [[name upper lower range] row]]
-        ^{:key name}
-          [:tr
-           [:td name]
-           [:td upper]
-           [:td lower]
-           [:td range]])]]]])
+  [:table.table.table-striped.table-bordered {:style {:width 1000 :margin-left 20}}
+    [:thead
+     [:tr
+      [:th "Current FTP"]
+      [:th {:colSpan "3"} "Watts"]]
+     [:tr
+      [:th "Zone-Purpose"]
+      [:th "Lower"]
+      [:th "Upper"]
+      [:th "% of FTP"]]]
+    [:tbody
+      (for [row input
+            :let [[name upper lower range] row]]
+      ^{:key name}
+        [:tr
+         [:td name]
+         [:td upper]
+         [:td lower]
+         [:td range]])]])
+
+(defn get-ftp []
+  (GET "/ftp"
+        {:handler #(.log js/console (str "response:" %))
+         :response-format :json}))
 
 (defn zone-page []
-   [:div
-     [ftp-component]
-     [table (zones/calc-power (:zones (:method @ftp-state)) (:ftp @ftp-state))]])
+  [:div.row
+   [input-comp]
+   [table (zones/calc-power zones/coggin @ftp)]])
 
 
 
